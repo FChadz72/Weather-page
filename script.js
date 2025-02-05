878ec71a6a715374a6b64f1d80c7bd67d089635f
FChadz72 <farai.chadzimura76@law.ac.uk>
2025-02-05 06:27:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function displayHourlyForecast(hourlyData) {
     next24Hours.forEach(item => {
         const dateTime = new Date(item.dt * 1000);
         const hour = dateTime.getHours();
-        const temperature = Math.round(item.main.temp);
+        const temperature = Math.round(item.main.temp - 273.15);
         const iconCode = item.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
 
